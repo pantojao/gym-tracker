e204214347cef5aeea4badcf4be1b60bc1d3e846
Joel Pantoja <pantojao@mac.local>
2022-10-23 15:17:23 -0700

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+// src/pages/_app.tsx
+import "../styles/globals.css";
+import type { AppType } from "next/app";
+import type { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default MyApp;
